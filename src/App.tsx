77ce9177cbaeaf6719d4362e896167466b61073f
arianna choza <unachoza@gmail.com>
2025-06-04 16:15:33 -0700

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import ParfumProductCard from "./components/newbie/ParfumProductCard/ParfumProductCard";
 import "./App.css";
 
 function App() {
 	return (
 		<>
 			<h1>Components</h1>
-			{/* <ParfumProductCard /> */}
 		</>
 	);
 }
