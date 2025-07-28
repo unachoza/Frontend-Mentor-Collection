714ea096aafa45f0c0d0b10ffadca614b345f2c1
arianna choza <unachoza@gmail.com>
2025-07-28 12:57:51 -0700

    hero and nav with links

--- src/components/junior/AgencyLandingPage/AgencyLandingPage.tsx
+++ src/components/junior/AgencyLandingPage/AgencyLandingPage.tsx
@@ -1,3 +1,4 @@
+import logo from "./images/logo.svg";
 import orangeSliceHeaderImage from "./images/desktop/image-header.jpg";
 import eggRow1Image from "./images/desktop/image-transform.jpg";
 import chaliceRow2Image from "./images/desktop/image-stand-out.jpg";
@@ -23,16 +24,29 @@ const AgencyLandingPage = () => {
 	return (
 		<div className={classes.main}>
 			<div className={classes.hero}>
-				{" "}
-				hero
+				<div className={`${classes["nav-container"]}`}>
+					<div className={classes.nav}>
+						<img src={logo} className={classes.logo} alt="logo" />
+						<ul className={`${classes["nav-links"]}`}>
+							<li>About</li>
+							<li>Services</li>
+							<li>Projects</li>
+							<li className={classes.active}>Contacts</li>
+						</ul>
+					</div>
+					<div className={classes.title}>We are creatives</div>
+				</div>
 				<img src={orangeSliceHeaderImage} alt="orange" />
 			</div>
 			<div className={`${classes["grid-container"]}`}>
 				{/* <!-- Row 1 --> */}
 				<div className={`${classes["text-box"]} ${classes["item"]}`}>
-					<div className={classes.title}></div>
-					<div className={classes.copy}></div>
-					<div className={classes.link}></div>
+					<div className={classes.title}>Transform your brand</div>
+					<div className={classes.copy}>
+						We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through
+						compelling visuals that do most of the marketing for you.
+					</div>
+					<div className={classes.link}> Learn more</div>
 				</div>
 				<div className={classes.item}>
 					<img src={eggRow1Image} alt="egg" />
@@ -43,9 +57,12 @@ const AgencyLandingPage = () => {
 					<img src={chaliceRow2Image} alt="chalice" />
 				</div>
 				<div className={`${classes["text-box"]} ${classes["item"]}`}>
-					<div className={classes.title}></div>
-					<div className={classes.copy}></div>
-					<div className={classes.link}></div>
+					<div className={classes.title}>Stand out to the right audience</div>
+					<div className={classes.copy}>
+						Using a collaborative formula of designers, researchers, photographers videographers, and copywriters, we'll build
+						and extend your brand in digital places.
+					</div>
+					<div className={classes.link}> Learn more</div>
 				</div>
 
 				{/* <!-- Row 3 --> */}
